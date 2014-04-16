b662fe49e97fe3ca8f4c0bfde6941c64e4a86a0a
Ronny Wang <ronnywang@gmail.com>
2014-04-17 01:03:52 +0800

    首頁改成在程式端做 group

--- webdata/models/Table.php
+++ webdata/models/Table.php
@@ -11,4 +11,21 @@ class Table extends Pix_Table
         $this->_columns['meta'] = array('type' => 'json');
         $this->_columns['tables'] = array('type' => 'json');
     }
+
+    public function getGroupedTables()
+    {
+        $tables = array();
+        foreach (Table::search(1) as $table) {
+            $meta = json_decode($table->meta);
+            if (!array_key_exists($meta->file, $tables)) {
+                $tables[$meta->file] = array();
+            }
+            $tables[$meta->file][$meta->page] = $table;
+        }
+
+        foreach ($tables as $file => $file_tables) {
+            ksort($tables[$file]);
+        }
+        return $tables;
+    }
 }
